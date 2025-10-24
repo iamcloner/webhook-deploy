64a8cb818524ecd27770abf0f34b1bf0996f397d
iamCloner <mohsen12rezaee120@gmail.com>
2025-10-24 11:19:34 +0330

    init

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM python:3.11-slim
+
+RUN apt-get update && apt-get install -y bash git && rm -rf /var/lib/apt/lists/*
+
+WORKDIR /app
+
+COPY . /app
+
+RUN pip install flask
+
+CMD ["python", "app.py"]
---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request
+import os
+import subprocess
+import glob
+
+app = Flask(__name__)
+
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    try:
+        deploy_dir = "/app/Deploy"  # در Docker مسیر داخل کانتینر است
+
+        sh_files = sorted(glob.glob(os.path.join(deploy_dir, "*.sh")))
+
+        if not sh_files:
+            return "No .sh files found in Deploy directory", 404
+
+        for script in sh_files:
+            subprocess.run(["chmod", "+x", script])
+            subprocess.run(["/bin/bash", script], check=True)
+
+        return "All deploy scripts executed successfully!", 200
+
+    except subprocess.CalledProcessError as e:
+        return f"Error executing script: {e}", 500
+    except Exception as e:
+        return f"Unexpected error: {e}", 500
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=7787)
